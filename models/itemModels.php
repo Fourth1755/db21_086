f9f9c34c04596d7eb10da26728d0fb42aea9b6c0
Fourth1755 <fourthe.hero@gmail.com>
2021-10-09 15:11:52 +0700

    Update itemModels.php

--- models/itemModels.php
+++ models/itemModels.php
@@ -24,9 +24,9 @@
         }
         public static function add($name,$detail){
             require("connection_connect.php");
-            $sql = "SELECT COUNT(Item_ID) FROM Item";
-            $result=$conn->query($sql);
-            echo $result;
+            $sql0 = "SELECT COUNT(Item_ID) FROM Item";
+            $result0=$conn->query($sql0);
+            echo $result0;
             $sql ="INSERT INTO Item (Item_ID,Item_Name,Item_Detail)
             VALUES('$id+1','$name','$detail')";
             $result=$conn->query($sql);
