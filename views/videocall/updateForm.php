de60b3041978bcd4f2ac754e0bbd0d8c3026aed4
Fourth1755 <fourthe.hero@gmail.com>
2021-10-11 20:56:02 +0700

    Update updateForm.php

--- views/videocall/updateForm.php
+++ views/videocall/updateForm.php
@@ -45,8 +45,7 @@
                         echo "<option selected disabled hidden>Open this select Color</option>";
                     }
                     else{
-                        echo "<option selected disabled hidden value=$videocall->color";
-                        echo ">$videocall->color</option>";
+                        echo "<option selected disabled hidden value=$videocall->color>$videocall->color</option>";
                     }
                 ?>
                 <option value="เขียว">เขียว</option>
