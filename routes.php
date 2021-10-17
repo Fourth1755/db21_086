4203fc3fe8636a1eeaaa0425997582540e0b03d0
Fourth1755 <fourthe.hero@gmail.com>
2021-10-17 18:48:03 +0700

    Update routes.php

--- routes.php
+++ routes.php
@@ -2,7 +2,7 @@
     $controllers =array('pages'=>['home', 'error'],
     'item'=>['index','newItem','addItem','deleteConfirm','delete','search','updateForm','updateItem'],
     'videocall'=>['index','newVideocall','addVideocall','deleteConfirm','delete','search','updateForm','updateVideocall'],
-    'necessities'=>['index','newNecessities']
+    'necessities'=>['index','newNecessities','addNecessities']
     );
     function call($controller, $action){
         require_once("controllers/".$controller."_controller.php");
