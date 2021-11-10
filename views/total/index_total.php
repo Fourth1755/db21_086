8a3fc0d80c1a0976ba7bc8a45c4f523e27be9033
Fourth1755 <fourthe.hero@gmail.com>
2021-11-10 15:01:50 +0700

    Update index_total.php

--- views/total/index_total.php
+++ views/total/index_total.php
@@ -21,21 +21,16 @@
     </div>
     </nav>
         <table class="table">
-        <tr><td>Name</td><td>Lastname</td><td>Homeisolation ID</td><td>Date</td><td>Necessities ID</td><td>Item Name</td><td>Quantity</td>
+        <tr><td>No.</td><td>Name</td><td>Lastname</td><td>Homeisolation ID</td><td>Date</td><td>Necessities ID</td><td>Item Name</td><td>Quantity</td>
         </tr>
         <?php
+            $i=0;
             foreach($total_list as $tot){
-                if(prev($tot->fname)==$tot->fname){
-                    echo "<tr><td></td>
-                    <td></td>";
-                }
-                else{
-                    echo "<tr><td>$tot->fname</td>
-                    <td>"; 
-                    echo  prev($tot->fname); "</td>";
-                }
-            echo
-            "<td>$tot->homeisolationID</td>
+
+            echo "<tr><td>$i</td>
+            <td>$tot->fname</td>
+            <td>$tot->lname</td>
+            <td>$tot->homeisolationID</td>
             <td>$tot->date</td>
             <td>$tot->necessitiesID</td>
             <td>$tot->itemName</td>
