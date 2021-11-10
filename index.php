3f3a078718f8f25b892e24d3e71bd16c65238221
Fourth1755 <fourthe.hero@gmail.com>
2021-11-10 16:48:37 +0700

    ตกแต่ง

--- index.php
+++ index.php
@@ -17,6 +17,7 @@
     <style>
       @import url('https://fonts.googleapis.com/css2?family=Prompt:wght@200;400;500&display=swap');
     </style>
+    <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
--- views/item/index_item.php
+++ views/item/index_item.php
@@ -5,7 +5,64 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="styles2.css">
+    <style>
+        .grid{
+    margin: 80px;
+    display: grid;
+    grid-template-columns: repeat(3,1fr);
+    grid-gap: 30px;
+    align-items: center;
+}
+.grid article{
+    background-color: #eee5e9;
+    border: none;
+    box-shadow: 2px 2px 6px 0px rgba(0,0,0,0.3);
+    border-radius: 20px;
+    text-align: center;
+    width: 250px;
+    transition:transform .3s;
+}
+.grid article:hover{
+    transform: translateY(5px);
+    box-shadow:  2px 2px 26px 0px rgba(0,0,0,0.3);
+}
+.grid article img{
+    width: 100%;
+    border-top-left-radius: 20px;
+    border-top-right-radius: 20px;
+}
+.text{
+    padding: 0 20px 20px;
+}
+.text h3{
+    text-transform: uppercase;
+}
+.text button{
+    background: #ef6f6c;
+    border-radius: 20px;
+    border: none;
+    color: #fff;
+    padding: 10px;
+    width: 100%;
+    font-weight: 600;
+    text-transform: uppercase;
+    cursor: pointer;
+}
+@media(max-width:768px){
+    .grid{
+        grid-template-columns: repeat(2,1fr);
+    }
+}
+@media(max-width:500px){
+    .grid{
+        grid-template-columns: repeat(1,1fr);
+    }
+    .grid article{
+        text-align: center;
+        
+    }
+}
+    </style>
     <title>Item</title>
 </head>
 <body >
@@ -14,7 +71,6 @@
     <nav class="navbar navbar-light bg-light">
     <div class="container-fluid">
     <a class="navbar-brand">Item</a>
-    <a class= "btn btn-success" href=?controller=item&action=newItem>Add new</a>
     <form class="d-flex" method="get" action="">
         <input class="form-control me-2" type="text" placeholder="Search" aria-label="Search" name="key">
         <input type="hidden" name="controller" value="item">
@@ -22,32 +78,15 @@
     </form>
     </div>
     </nav>
-        <table class="table">
-        <!-- <tr><td>ID</td><td> </td><td>Item</td><td>Item Detail</td>
-        <td>Update</td><td>Delete</td></tr> -->
-        <?php
-            foreach($item_list as $item){
-            echo "<tr><td>$item->id</td>
-                <td><img src=$item->image alt=image style=width:80px;height:80px;></td>
-                <td>$item->name</td>
-                <td>$item->detail</td>
-                ";?>
-                <td><a type="button" class="btn btn-primary"href=?controller=item&action=updateForm&<?php echo "id=$item->id";?>><i class="material-icons">build</i></a></td>
-                <td><a class="btn btn-danger"href=?controller=item&action=deleteConfirm&<?php echo "itemID=$item->id";?>><i class="material-icons">delete</i></a></td>
-                </tr>
-            <?php
-            }    
-            echo "</table>";
-        ?>
         <div class ="container">
           <main class="grid">
           <?php
-            foreach($item_list as $item){
-              echo "<article>
-                  <img src=$item->image>
-                  <div class=text>
-                      <h3>$item->name</h3>
-                      <p>$item->detail</p>"?>
+            foreach($item_list as $item){?>
+              <article>
+                  <img <?php echo "src= $item->image";?>>
+                  <div class="text">
+                      <h3><?php echo $item->name?></h3>
+                      <p><?php echo $item->detail?></p>
                       <a type="button" class="btn btn-primary"href=?controller=item&action=updateForm&<?php echo "id=$item->id";?>><i class="material-icons">build</i></a>
                       <a class="btn btn-danger"href=?controller=item&action=deleteConfirm&<?php echo "itemID=$item->id";?>><i class="material-icons">delete</i></a>
                     </div>
