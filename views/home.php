78e8ef54d606738b50ce61065305d1b22a240765
Fourth1755 <fourthe.hero@gmail.com>
2021-11-11 09:03:03 +0700

    Update home.php

--- views/home.php
+++ views/home.php
@@ -121,6 +121,7 @@
     <div class="row">
         <div class="col-1">
             <h2>Pooriwat<br>Poolab</h2>
+            <h3>6220504755</h3>
             <h3>Item and Video Call</h3>
             <p>เจ้าหน้าที่home isolation โทรสอบถามข้อมูลความต้องการ 
                 โดยบันทึกข้อมูลสิ่งของที่จำเป็น สำหรับส่งไปยังที่อยู่ที่ผู้ป่วยทำ 
